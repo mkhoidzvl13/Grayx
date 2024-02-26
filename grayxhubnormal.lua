4be47f1e503a66b3835512b740e1a9af30508be4
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2024-02-26 19:10:50 +0700

    Update grayxhubnormal.lua

--- grayxhubnormal.lua
+++ grayxhubnormal.lua
@@ -13,7 +13,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(530, 350),
     Acrylic = true,
-    Theme = "Aqua",
+    Theme = "Rose",
     MinimizeKey = Enum.KeyCode.End
 })
 local Tabs = {

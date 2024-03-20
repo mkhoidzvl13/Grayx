530b35e92387dd74b61f6c129c4e015a0e094ec6
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2024-03-20 19:45:16 +0700

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,100 +1,224 @@
-function GetBeli()
-    local beli = game.Players.LocalPlayer.Data.Beli.Value
-    if beli >= 1e6 then
-        return tostring(beli/1e6) .. "M"
-    elseif beli >= 1e3 then
-        return tostring(beli/1e3) .. "K"
-    else
-        return tostring(beli)
-    end
-end
-function GetFragments()
-    local Fragments = game.Players.LocalPlayer.Data.Fragments.Value
-    if Fragments >= 1e6 then
-        return tostring(Fragments/1e6) .. "M"
-    elseif Fragments >= 1e3 then
-        return tostring(Fragments/1e3) .. "K"
-    else
-        return tostring(Fragments)
-    end
-end
-function GetSword()
-    local SwordList = {}
-    for k, v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v["Type"] == "Sword" then
-            table.insert(SwordList,v.Name.." : "..v["Mastery"])
-        end
-    end
-    local SwordListC = table.concat(SwordList,",")
-    return SwordListC
-end
-function GetGun()
-    local GunList = {}
-    for k, v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v["Type"] == "Gun" then
-            table.insert(GunList,v.Name.." : "..v["Mastery"])
-        end
-    end
-    local GunListC = table.concat(GunList,",")
-    return GunListC
-end
-function CheckMaterialCount()
-    local MaterialsInventoryList = {}
-    for k, v in pairs(game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")) do
-        if v["Type"] == "Material" then
-            table.insert(MaterialsInventoryList,v.Name.." : "..v["Count"])
-        end
-    end
-    local MaterialC = table.concat(MaterialsInventoryList,"\n")
-    return MaterialC
-end
-local CTXWebhookfunc = {
-    ["content"] = "\67\84\88\32\65\112\105\32\76\111\97\100\105\110\103\115",
-    ["embeds"] = {
-        {
-            ["color"] = 15466240,
-            ["fields"] = {
-                {
-                    ["name"] = "\80\108\97\121\101\114\115\32\45\32\73\68\32\80\108\97\121\101\114\115",
-                    ["value"] = "```yml\n"..game.Players.LocalPlayer.Name.." - "..game.Players.LocalPlayer.UserId.."\n```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "\76\101\118\101\108\32\45\32\66\101\108\105\32\45\32\70\114\97\103",
-                    ["value"] = "```yml\n"..game.Players.LocalPlayer.Data.Level.Value.." - "..GetBeli().." - "..GetFragments().."\n```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "\83\119\111\114\100",
-                    ["value"] = "```yml\n"..GetSword().."\n```"
-                },
-                {
-                    ["name"] = "\71\117\110",
-                    ["value"] = "```yml\n"..GetGun().."\n```"
-                },
-                {
-                    ["name"] = "\77\97\116\101\114\105\97\108",
-                    ["value"] = "```yml\n"..CheckMaterialCount().."\n```"
-                }
-            },
-            ["author"] = {
-                ["name"] = "Blox Fruits"
-            },
-            ["footer"] = {
-                ["text"] = "Grayx hub",
-                ["icon_url"] = "https://cdn.discordapp.com/attachments/1218431024187445248/1219496916614905968/f5ec64b40d2adf7deb84e3c0e192ff32.gif?ex=660b8402&is=65f90f02&hm=a4dac3d27875cdf085add8a827cb818b8b10f6c52ab2a18a641838d78c1eb91d&"
-            },
-            ["timestamp"] = "2024-03-19T11:38:00.000Z",
-            ["image"] = {
-                ["url"] = "https://cdn.discordapp.com/attachments/1218431024187445248/1219496916614905968/f5ec64b40d2adf7deb84e3c0e192ff32.gif?ex=660b8402&is=65f90f02&hm=a4dac3d27875cdf085add8a827cb818b8b10f6c52ab2a18a641838d78c1eb91d&"
-            },
-            ["thumbnail"] = {
-                ["url"] = "https://cdn.discordapp.com/attachments/1218431024187445248/1219496916614905968/f5ec64b40d2adf7deb84e3c0e192ff32.gif?ex=660b8402&is=65f90f02&hm=a4dac3d27875cdf085add8a827cb818b8b10f6c52ab2a18a641838d78c1eb91d&"
-            }
-        }
-    },
-    ["username"] = "\67\84\88\32\87\101\98\104\111\111\107\32\65\112\105",
-    ["attachments"] = {}
+local cac = require(game:GetService("Players").LocalPlayer.PlayerGui.Main.UIController.Inventory)
+local Inventory = game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getInventory")
+local Items = {}
+local RaityLevel = {"Mythical","Legendary","Rare","Uncommon","Common"}
+local RaityColor =  {
+    ["Common"] = Color3.fromRGB(179, 179, 179),
+    ["Uncommon"] = Color3.fromRGB(92, 140, 211),
+    ["Rare"] =  Color3.fromRGB(140, 82, 255),
+    ["Legendary"] = Color3.fromRGB(213, 43, 228) ,
+    ["Mythical"] =  Color3.fromRGB(238, 47, 50)
 }
-local Send = http_request or request or HttpPost or syn.request
-Send({Url = UrlWebhook, Body = game:GetService("HttpService"):JSONEncode(CTXWebhookfunc), Method = "POST", Headers = {["content-type"] = "application/json"}})
+function GetRaity(color)
+    for k,v in pairs(RaityColor) do 
+        if v==color then return k end
+    end
+end
+
+for k,v in pairs(Inventory) do 
+    Items[v.Name] = v
+end
+
+local total = #getupvalue(cac.UpdateRender,4)
+local rac = {}
+local allitem = {}
+local total2 = 0
+while total2<total do 
+    local i = 0
+    while i < 25000 and total2<total do 
+        game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.CanvasPosition = Vector2.new(0,i)
+        for k,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.Frame:GetChildren()) do 
+            if v:IsA("Frame") and not rac[v.ItemName.Text] and v.ItemName.Visible==true then 
+                local vaihuhu = GetRaity(v.Background.BackgroundColor3)
+                if vaihuhu then
+                    print("Rac")
+                    if not allitem[vaihuhu] then 
+                        allitem[vaihuhu] = {}
+                    end
+                    table.insert(allitem[vaihuhu],v:Clone())
+                end
+                total2=total2+1
+                rac[v.ItemName.Text] = true
+            end
+        end
+        i=i+20
+    end
+    wait()
+end
+function GetXY(vec) 
+    return vec*100
+end
+
+local tvk = Instance.new("UIListLayout")
+tvk.FillDirection = Enum.FillDirection.Vertical
+tvk.SortOrder = 2
+tvk.Padding = UDim.new(0,20)
+
+local Left = Instance.new("Frame",game.Players.LocalPlayer.PlayerGui.BubbleChat)
+Left.BackgroundTransparency = 1
+Left.Size = UDim2.new(.5,0,1,0) 
+tvk.Parent = Left
+
+local Right = Instance.new("Frame",game.Players.LocalPlayer.PlayerGui.BubbleChat)
+Right.BackgroundTransparency = 1
+Right.Size = UDim2.new(.5,0,1,0) 
+Right.Position = UDim2.new(.6,0,0,0)
+Right.Name = "Right"
+tvk:Clone().Parent = Right
+local bucac
+for k,v in pairs(allitem) do 
+    local cac = Instance.new("Frame",Left)
+    cac.BackgroundTransparency = 1
+    cac.Size = UDim2.new(1,0,0,0) 
+    cac.LayoutOrder = table.find(RaityLevel,k)
+
+    local cac2 = Instance.new("Frame",Right)
+    cac2.BackgroundTransparency = 1
+    cac2.Size = UDim2.new(1,0,0,0) 
+    cac2.LayoutOrder = table.find(RaityLevel,k)
+ 
+
+    local tvk = Instance.new("UIGridLayout",cac)
+    tvk.CellPadding = UDim2.new(.005,0,.005,0)
+    tvk.CellSize =  UDim2.new(0,70,0,70)
+    tvk.FillDirectionMaxCells = 100
+    tvk.FillDirection = Enum.FillDirection.Horizontal
+
+    local ccc = tvk:Clone()
+    ccc.Parent = cac2
+    for k,v in pairs(v) do 
+        if Items[v.ItemName.Text] and Items[v.ItemName.Text].Mastery then 
+            if v.ItemLine2.Text~="Accessory" then 
+                bucac = v.ItemName:Clone()
+                bucac.BackgroundTransparency = 1
+                bucac.TextSize = 10
+                bucac.TextXAlignment  = 2
+                bucac.TextYAlignment  = 2
+                bucac.ZIndex  = 5
+                bucac.Text = Items[v.ItemName.Text].Mastery
+                bucac.Size = UDim2.new(.5,0,.5,0)
+                bucac.Position = UDim2.new(.5,0,.5,0)
+                bucac.Parent = v
+            end
+            v.Parent = cac
+        elseif v.ItemLine2.Text == "Blox Fruit" then 
+            v.Parent = cac2
+        end
+    end
+    cac.AutomaticSize = 2
+    cac2.AutomaticSize = 2
+end
+
+local MeleeG = Instance.new("Frame",Right)
+MeleeG.BackgroundTransparency = 1
+MeleeG.Size = UDim2.new(1,0,0,0) 
+MeleeG.LayoutOrder = table.find(RaityLevel,k)
+MeleeG.AutomaticSize=2
+MeleeG.LayoutOrder = 100
+local tvk = Instance.new("UIGridLayout",MeleeG)
+tvk.CellPadding = UDim2.new(.005,0,.005,0)
+tvk.CellSize =  UDim2.new(0,70,0,70)
+tvk.FillDirectionMaxCells = 100
+tvk.FillDirection = Enum.FillDirection.Horizontal
+local ListHuhu = {
+    ["Superhuman"] = Vector2.new(3,2),
+    ["DeathStep"] = Vector2.new(4,3),
+    ["ElectricClaw"] = Vector2.new(2,0),
+    ["SharkmanKarate"] = Vector2.new(0,0),
+    ["DragonTalon"] = Vector2.new(1,5),
+    ["Godhuman"] = "rbxassetid://16729397232"
+}
+local nguu = {}
+function GetNext() end
+local Listcaiditconmemayskidconcaca = {}
+local buda
+
+for k,v in pairs(ListHuhu) do
+    if game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy"..k,true) == 1 then 
+        local huhu = Instance.new("ImageLabel",MeleeG)
+        if type(v)=="string" then 
+            huhu.Image = v
+        else
+            huhu.Image = "rbxassetid://16729397232"
+            huhu.ImageRectSize = Vector2.new(100,100)
+            huhu.ImageRectOffset = v*100
+        end 
+        Listcaiditconmemayskidconcaca[k] = huhu
+        table.insert(nguu,k)
+    end
+end
+buda = 1
+function TimKiemItemNehuhu(item)
+    for k,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do 
+        if v.Name:gsub(" ","") == item then 
+            return v
+        end
+    end
+end
+spawn(function() 
+    local a = #nguu
+    local bucu = 0
+    while bucu < a do 
+        for k,v in pairs(Listcaiditconmemayskidconcaca) do 
+            if not v:FindFirstChild("Ditme") then 
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy"..k) 
+                wait(.1)
+                local v2 = TimKiemItemNehuhu(k)
+                if v2 then 
+                    v2:WaitForChild("Level")
+                    local Ditme = bucac:Clone()
+                    Ditme.Name = "Ditme"
+                    Ditme.BackgroundTransparency = 1
+                    Ditme.TextSize = 10
+                    Ditme.TextXAlignment  = 2
+                    Ditme.TextYAlignment  = 2
+                    Ditme.ZIndex  = 5
+                    Ditme.Text = v2.Level.Value
+                    Ditme.Size = UDim2.new(.5,0,.5,0)
+                    Ditme.Position = UDim2.new(.5,0,.5,0)
+                    Ditme.Parent = v
+                    bucu=bucu+1
+                end
+            end
+        end
+        wait()
+    end
+
+end)
+game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler.Visible = true
+repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler.TopContainer.Frame:FindFirstChild("Z")
+local rac = game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler:Clone()
+rac.LayoutOrder = 101
+game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler.Visible = false
+
+rac.Parent = Right
+rac.Size = UDim2.new(1,0,0.3,0)
+function formatNumber(v)
+    return tostring(v):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
+end
+local thieunang = game:GetService("Players").LocalPlayer.PlayerGui.Main.Fragments:Clone()
+thieunang.Parent = game:GetService("Players").LocalPlayer.PlayerGui.BubbleChat
+thieunang.Position = UDim2.new(0,6,0.85799,0)
+local n = formatNumber(game.Players.LocalPlayer.Data.Fragments.Value)
+thieunang.Text = "ƒ"..n
+print("Done")
+pcall(function() 
+    game:GetService("Players").LocalPlayer.PlayerGui.Main.MenuButton.Visible = false
+  -- game:GetService("Players").LocalPlayer.PlayerGui.Main.MenuButton:Destroy()
+end)
+pcall(function() 
+    game:GetService("Players").LocalPlayer.PlayerGui.Main.HP.Visible = false
+    --game:GetService("Players").LocalPlayer.PlayerGui.Main.HP:Destroy()
+end)
+pcall(function() 
+    game:GetService("Players").LocalPlayer.PlayerGui.Main.Energy.Visible = false
+    --game:GetService("Players").LocalPlayer.PlayerGui.Main.Energy:Destroy()
+end)
+for k,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main:GetChildren()) do 
+    if v:IsA("ImageButton") then 
+        v.Visible = false
+    end
+end
+pcall(function() 
+    game:GetService("Players").LocalPlayer.PlayerGui.Main.Compass:Destroy()
+end)
